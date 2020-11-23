42a03f9167afb4adb89300707198a6ce2066b62b
Talris <b_vitall@mail.ru>
2020-11-23 13:36:58 +0300

    1st commit

---
+++ Program.java
@@ -0,0 +1,9 @@
+
+
+public class Program {
+    public static void main(String[] args) {
+        String tekst = "привет мир!";
+        String n = tekst.toUpperCase();
+            System.out.println(n);
+    }
+}
